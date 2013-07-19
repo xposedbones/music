6d21164f5decaca7404a14a03b2ee410a4fa2215
Benoit Lavoie-Lamer <xposedbones@gmail.com>
2013-07-19 16:44:48 -0400

    header

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,4 @@
+</div>
 	<script src="js/jquery.min.js"></script>
 	<script src="js/script.min.js"></script>
 </body>
--- includes/header.php
+++ includes/header.php
@@ -12,3 +12,12 @@ if(!class_exists("Bd")){
 	
 </head>
 <body>
+<header>
+	<div><a href="#" class="icon-upload">Upload </a></div>
+	<ul>
+		<li><a href="#">My stuff</a></li>
+		<li><a href="#">Browse</a></li>
+		<li><a href="#">Meh</a></li>
+	</ul>
+</header>
+<div id="content">
